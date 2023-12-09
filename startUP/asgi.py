12f19c978addf32b035e0821d69ef2f81c12a1e0
abdulwahab alshehri <abdul.2020alsh@gmail.com>
2023-12-09 22:40:34 +0300

    starter PROJECT

--- startUP/asgi.py
+++ startUP/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for moc project.
+ASGI config for startUP project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moc.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'startUP.settings')
 
 application = get_asgi_application()
